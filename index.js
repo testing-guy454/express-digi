6ef86a8b1e78b92ffba2afde182a55c7119bfbb5
Yuvraj Mehta <yuvraj.mehta532@gmail.com>
2025-05-19 22:42:12 +0530

    added env

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+// require('dotenv').config()
+import 'dotenv/config'
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json())
 
 let teaData = [];
